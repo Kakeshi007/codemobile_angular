1ec1a185850a9e0add36f6a365cbea1c0a33e4ab
Wiriya Khetwit <noise@Wiriyas-MacBook-Pro.local>
2021-02-03 22:30:26 +0700

    complete slid nave width

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,27 @@
-import { Component } from '@angular/core';
-
+import { ChangeDetectorRef, Component, OnDestroy } from '@angular/core';
+import {MediaMatcher} from '@angular/cdk/layout';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'codingmobile';
+export class AppComponent implements OnDestroy {
 
-  name = 'wiriya';
+  mobileQuerMax: MediaQueryList;
+  
+  private _mobileQueryListener:()=>void;
 
-  position = ['Admin', 'HR', 'IT'];
+  constructor(changeDetectorRef: ChangeDetectorRef, 
+    media: MediaMatcher, 
+    )
+  {
+    this._mobileQueryListener=()=>changeDetectorRef.detectChanges();
+    this.mobileQuerMax = media.matchMedia('(max-width:600px)');
+    this.mobileQuerMax.addListener(this._mobileQueryListener);
+  }
+  ngOnDestroy(): void {
+    this.mobileQuerMax.removeListener(this._mobileQueryListener);
+  }
 
   passalert(message: any){
     console.log(message);
